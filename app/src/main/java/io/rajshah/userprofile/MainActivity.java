b9acc501ba606e139d9bcfe9b9a82068108f2339
Raj Shah <rshah62@uncc.edu>
2021-09-13 05:53:21 -0400

    ✨: New : ActionBar Config and Var Set

--- app/src/main/java/io/rajshah/userprofile/MainActivity.java
+++ app/src/main/java/io/rajshah/userprofile/MainActivity.java
@@ -19,5 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setActionBarConfig();
+        editText_Name=findViewById(R.id.editText_Name);
+        editText_ID=findViewById(R.id.editText_ID);
+        editText_Email=findViewById(R.id.editText_Email);
+
     }
 }
