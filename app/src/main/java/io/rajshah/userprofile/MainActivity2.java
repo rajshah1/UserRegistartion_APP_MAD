a69144c315efe37b68d669cb05295ca20527c49f
Raj Shah <rshah62@uncc.edu>
2021-09-17 11:11:42 -0400

    :Update : Main Activity 2 added for data

--- app/src/main/java/io/rajshah/userprofile/MainActivity2.java
+++ app/src/main/java/io/rajshah/userprofile/MainActivity2.java
@@ -3,15 +3,21 @@ package io.rajshah.userprofile;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
     RadioGroup radioGroup_Dept;
     RadioButton radioButton_CS,radioButton_DS,radioButton_Bio,radioButton_SIS;
+    public static final int Activity_Cancel = 300;
+    String selectedProgram;
+    public static String ENTERED_DATA = "Entered_Data";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +28,24 @@ public class MainActivity2 extends AppCompatActivity {
         radioButton_SIS=findViewById(R.id.radioButton_SIS);
         radioButton_Bio=findViewById(R.id.radioButton_Bio);
         radioButton_DS=findViewById(R.id.radioButton_DS);
+
+        radioGroup_Dept.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == R.id.radioButton_CS){
+                    selectedProgram=radioButton_CS.getText().toString();
+                }
+                else if (i == R.id.radioButton_Bio){
+                    selectedProgram=radioButton_Bio.getText().toString();
+                }
+                else if (i == R.id.radioButton_DS){
+                    selectedProgram=radioButton_DS.getText().toString();
+                }
+                else if (i == R.id.radioButton_SIS){
+                    selectedProgram=radioButton_SIS.getText().toString();
+                }
+            }
+        });
     }
     private void setActionBarConfig() {
         ActionBar aBar = getSupportActionBar();
@@ -29,5 +53,20 @@ public class MainActivity2 extends AppCompatActivity {
         assert aBar != null;
         aBar.setBackgroundDrawable(cd);
         aBar.setTitle(R.string.Second_ActionBar);
+    }
+
+    public void sendDataBack(View view){
+        if (selectedProgram == null || selectedProgram.length() == 0) {
+            Toast.makeText(MainActivity2.this, "Please Select One Department", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent();
+            intent.putExtra(ENTERED_DATA,selectedProgram);
+            setResult(RESULT_OK,intent);
+            finish();
+        }
+    }
+    public void closeActivity(View view){
+        setResult(Activity_Cancel);
+        finish();
     }
 }
